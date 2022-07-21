f989c059933ee7fe619ca8bb5894301bf0d8cfed
Nigger Hunter 1337 <Nigger Hunter 1337@DESKTOP-FFUSID1>
2022-07-21 17:37:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Gross Pay(IDR): ");
+            decimal action = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("The net pay is: " + Salary.Salary_calculate(action) + " IDR");
+        }
+    }
+}
---
+++ Salary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Salary
+    {
+        public static decimal Salary_calculate(decimal gross)
+        {
+            decimal net;
+            if (gross > 3000)
+            {
+                net = gross - ((gross - 1000) / 10) - (2000 * 15 / 100);
+                return net;
+            }
+            else if (gross > 1000 && gross < 3000)
+            {
+                net = gross - ((gross - 1000) / 10) - ((gross - 1000) * 15 / 100);
+                return net;
+            }
+                
+            return net = gross;
+        }
+    }
+}
